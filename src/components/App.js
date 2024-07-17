fb2431fac11115665c27be004da38613d0a035ef
Paul Donovan <paul.m.donovan@outlook.com>
2024-07-17 20:08:33 +1000

    startsApp

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+// import React from 'react';
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [dog, setDog] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setDog(data.breeds);
+                setIsLoaded(true);
+            })
+    }, [])
+    if (!isLoaded) return <p> Loading...</p>
+
+    return (
+        <div>
+            <img src={data.breeds.image} alt="A Random Dog" />
+        </div>
+    )
+        ;
+}
+
+export default App;
